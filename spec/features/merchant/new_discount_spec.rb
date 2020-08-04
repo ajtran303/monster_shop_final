3ce2cbee0875d334c257e4174624ac86784e9f3b
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-08-04 14:30:00 -0600

    Add new discount creation spec

---
+++ spec/features/merchant/new_discount_spec.rb
@@ -0,0 +1,72 @@
+require "rails_helper"
+
+RSpec.describe "New Merchant Bulk Discount Spec" do
+  before :each do
+    @merchant1 = create(:merchant)
+    @discount1 = create(:discount, merchant: @merchant1)
+    @employee1 = create(:employee, merchant: @merchant1, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@employee1)
+  end
+
+  describe "As a merchant employee" do
+    describe "On my bulk discount index page" do
+      it "There is a link to create a new bulk discount" do
+        visit merchant_discounts_path
+        expect(page).to have_link("Create New Bulk Discount", href: "/merchant/discounts/new")
+      end
+    end
+
+    it "Filling the form out completely will create a new bulk discount" do
+      visit new_merchant_discount_path
+      expect(BulkDiscount.count).to eq(1)
+
+      fill_in :bulk_quantity, with: 40
+      fill_in :percentage_discount, with: 10
+      click_button "Create Bulk Discount"
+
+      expect(BulkDiscount.count).to eq(2)
+      expect(current_path).to eq(merchant_discounts_path)
+    end
+
+    describe "Form validation: I cannot make a new discount after" do
+      before :each do
+        visit new_merchant_discount_path
+      end
+
+      it "Failing to fill out the form completely" do
+        expect(BulkDiscount.count).to eq(1)
+
+        # fill_in :bulk_quantity, with: 40
+        fill_in :percentage_discount, with: 10
+        click_button "Create Bulk Discount"
+
+        expect(BulkDiscount.count).to eq(1)
+        expect(current_path).to eq(new_merchant_discount_path)
+
+
+        fill_in :bulk_quantity, with: 40
+        # fill_in :percentage_discount, with: 10
+        click_button "Create Bulk Discount"
+
+        expect(BulkDiscount.count).to eq(1)
+        expect(current_path).to eq(new_merchant_discount_path)
+      end
+
+      it "Filling in the form with invalid information" do
+        fill_in :bulk_quantity, with: -1
+        fill_in :percentage_discount, with: 10
+        click_button "Create Bulk Discount"
+
+        expect(BulkDiscount.count).to eq(1)
+        expect(current_path).to eq(new_merchant_discount_path)
+
+        fill_in :bulk_quantity, with: 40
+        fill_in :percentage_discount, with: -10
+        click_button "Create Bulk Discount"
+
+        expect(BulkDiscount.count).to eq(1)
+        expect(current_path).to eq(new_merchant_discount_path)
+      end
+    end
+  end
+end
