b4f4a33c310b23d3e55b0877fa2fb30e5a712f66
AJ Tran <ajtrandev@gmail.com>
2020-08-05 00:40:43 -0600

    Add BulkDiscount::find_eligible_discount()

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,8 @@ class BulkDiscount < ApplicationRecord
 
   validates_inclusion_of :percentage_discount, in: 1..100, message:  "Discount must be percentage from 1 - 100"
   validates :bulk_quantity, numericality: { greater_than: 0 }
+
+  def self.find_eligible_discount(item_quantity)
+    order(:bulk_quantity).where("bulk_quantity <= ?", item_quantity).last
+  end
 end
