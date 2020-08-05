dfba11f29b7d8c3845f17e0845470f047ccfeb0b
AJ Tran <ajtrandev@gmail.com>
2020-08-05 01:43:11 -0600

    Update apply_discount_in_cart_spec

--- spec/features/cart/apply_discount_in_cart_spec.rb
+++ spec/features/cart/apply_discount_in_cart_spec.rb
@@ -8,9 +8,13 @@ RSpec.describe "Apply Discount in Cart Spec" do
     @item1 = create(:item, merchant: @merchant1, price: 10, inventory: 10)
     @item2 = create(:item, merchant: @merchant1)
     @discount1 = create(:discount, merchant: @merchant1, bulk_quantity: 5, percentage_discount: 10)
+    @discount2 = create(:discount, merchant: @merchant1, bulk_quantity: 10, percentage_discount: 20)
 
-    @discount_notice = "Discount applied: #{@discount1.percentage_discount}% off for #{pluralize(@discount1.bulk_quantity, @item1.name)}!"
-    @discounted_subtotal = "Subtotal: #{number_to_currency((@item1.price * 5) * 0.01 * (100 - @discount1.percentage_discount))}"
+    @discount_notice1 = "Discount applied: #{@discount1.percentage_discount}% off for #{pluralize(@discount1.bulk_quantity, @item1.name)}!"
+    @discounted_subtotal1 = "Subtotal: #{number_to_currency(@discount1.calculate_discount(@item1.price * 5))}"
+
+    @discount_notice2 = "Discount applied: #{@discount2.percentage_discount}% off for #{pluralize(@discount2.bulk_quantity, @item1.name)}!"
+    @discounted_subtotal2 = "Subtotal: #{number_to_currency(@discount2.calculate_discount(@item1.price * 10))}"
 
     [@item1, @item2].each do |item|
       4.times do
@@ -27,7 +31,7 @@ RSpec.describe "Apply Discount in Cart Spec" do
       it "The price is adjusted with a percentage discount" do
         within "#item-#{@item1.id}" do
           expect(page).to have_content("Subtotal: #{number_to_currency(@item1.price * 4)}")
-          expect(page).to_not have_content(@discount_notice)
+          expect(page).to_not have_content(@discount_notice1)
         end
 
         visit item_path(@item1)
@@ -36,8 +40,8 @@ RSpec.describe "Apply Discount in Cart Spec" do
 
         # discount for item above the bulk_quantity
         within "#item-#{@item1.id}" do
-          expect(page).to have_content(@discounted_subtotal)
-          expect(page).to have_content(@discount_notice)
+          expect(page).to have_content(@discounted_subtotal1)
+          expect(page).to have_content(@discount_notice1)
         end
 
         # no discount for item below the bulk_quantity
@@ -46,52 +50,90 @@ RSpec.describe "Apply Discount in Cart Spec" do
           expect(page).to_not have_content("Discount applied")
         end
       end
+
+      it "Bulk discounts only apply to specific merchants with that discount" do
+        merchant2 = create(:merchant)
+        item3 = create(:item, merchant: merchant2)
+
+        5.times do
+          visit item_path(item3)
+          click_button "Add to Cart"
+        end
+
+        visit "/cart"
+
+        within "#item-#{item3.id}" do
+          expect(page).to have_content("Subtotal: #{number_to_currency(item3.price * 5)}")
+          expect(page).to_not have_content("Discount applied")
+        end
+      end
     end
 
     describe "When I adjust the quantity of items in my cart" do
-      it "The discount is automatically added" do
+      it "The discount is automatically added when I meet the bulk quantity" do
         within "#item-#{@item1.id}" do
           expect(page).to have_content("Subtotal: #{number_to_currency(@item1.price * 4)}")
-          expect(page).to_not have_content(@discount_notice)
+          expect(page).to_not have_content(@discount_notice1)
 
           click_button("More of This!")
 
-          expect(page).to have_content(@discounted_subtotal)
-          expect(page).to have_content(@discount_notice)
+          expect(page).to have_content(@discounted_subtotal1)
+          expect(page).to have_content(@discount_notice1)
         end
       end
 
-      it "The discount is automatically removed" do
+      it "The discount is automatically removed when I no longer meet the bulk quantity" do
         visit item_path(@item1)
         click_button "Add to Cart"
         visit "/cart"
 
         within "#item-#{@item1.id}" do
-          expect(page).to have_content(@discounted_subtotal)
-          expect(page).to have_content(@discount_notice)
+          expect(page).to have_content(@discounted_subtotal1)
+          expect(page).to have_content(@discount_notice1)
 
           click_button("Less of This!")
 
           expect(page).to have_content("Subtotal: #{number_to_currency(@item1.price * 4)}")
-          expect(page).to_not have_content(@discount_notice)
+          expect(page).to_not have_content(@discount_notice1)
         end
       end
-    end
 
-    it "Bulk discounts only apply to specific merchants with that discount" do
-      merchant2 = create(:merchant)
-      item3 = create(:item, merchant: merchant2)
+      it "The discount is automatically adjusted when I meet a higher bulk quantity" do
+        within "#item-#{@item1.id}" do
+          click_button("More of This!")
 
-      5.times do
-        visit item_path(item3)
-        click_button "Add to Cart"
+          expect(page).to have_content(@discounted_subtotal1)
+          expect(page).to have_content(@discount_notice1)
+        end
+
+        within "#item-#{@item1.id}" do
+          5.times { click_button("More of This!") }
+
+          expect(page).to have_content(@discounted_subtotal2)
+          expect(page).to have_content(@discount_notice2)
+
+          expect(page).to_not have_content(@discounted_subtotal1)
+          expect(page).to_not have_content(@discount_notice1)
+        end
       end
 
-      visit "/cart"
+      it "The discount is automatically adjusted when I meet a lower bulk quantity" do
+        within "#item-#{@item1.id}" do
+          6.times { click_button("More of This!") }
 
-      within "#item-#{item3.id}" do
-        expect(page).to have_content("Subtotal: #{number_to_currency(item3.price * 5)}")
-        expect(page).to_not have_content("Discount applied")
+          expect(page).to have_content(@discounted_subtotal2)
+          expect(page).to have_content(@discount_notice2)
+        end
+
+        within "#item-#{@item1.id}" do
+          5.times { click_button("Less of This!") }
+
+          expect(page).to have_content(@discounted_subtotal1)
+          expect(page).to have_content(@discount_notice1)
+
+          expect(page).to_not have_content(@discounted_subtotal2)
+          expect(page).to_not have_content(@discount_notice2)
+        end
       end
     end
   end
